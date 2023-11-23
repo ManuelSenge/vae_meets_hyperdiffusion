c3eb57fffc255f1eddd5a0c23b70568e12f7110e
Manuel Senge <manuelsenge@Manuels-MBP.fritz.box>
2023-11-23 23:57:12 +0100

    fixed vae eval

--- vae/create_mesh_from_VAE.py
+++ vae/create_mesh_from_VAE.py
@@ -36,30 +36,33 @@ else:
 
 
 mlp_kwargs = OmegaConf.create({'model_type':'mlp_3d', 'out_size':1, 'hidden_neurons':[128, 128, 128], 'output_type':'occ', 'out_act':'sigmoid', 'multires':4, \
-    'use_leaky_relu':False, 'move':False})
+    'use_leaky_relu':False, 'move':False, 'device':'mps'})
 
 i = 0
 mesh_dir = "gen_meshes/VAE"
 
-def generate_meshes(weights, folder_name="meshes", info="0", res=64, level=0):
-    meshes = []
-    sdfs = []
-    for i, w in enumerate(weights):
-        mlp = generate_mlp_from_weights(w, mlp_kwargs)
-        sdf_decoder = SDFDecoder(
-            mlp_kwargs.model_type,
-            None,
-            "nerf" if mlp_kwargs.model_type == "nerf" else "mlp",
-            mlp_kwargs
-        )
-        sdf_decoder.model = mlp.to(device).eval()
-        with torch.no_grad():
-            effective_file_name = (
-                f"{folder_name}/mesh_{i}__{info}"
-                if folder_name is not None
-                else None
+def generate_meshes(x_0s, folder_name="meshes", info="0", res=64, level=0):
+        x_0s = x_0s.view(len(x_0s), -1)
+        curr_weights = None
+        x_0s = x_0s[:, :curr_weights]
+        meshes = []
+        sdfs = []
+        for i, weights in enumerate(x_0s):
+            mlp = generate_mlp_from_weights(weights, mlp_kwargs)
+            sdf_decoder = SDFDecoder(
+                mlp_kwargs.model_type,
+                None,
+                "nerf" if mlp_kwargs.model_type == "nerf" else "mlp",
+                mlp_kwargs
             )
-
+            sdf_decoder.model = mlp.to(device).eval()
+            with torch.no_grad():
+                effective_file_name = (
+                    f"{folder_name}/mesh_epoch_{i}_{info}"
+                    if folder_name is not None
+                    else None
+                )
+                
             v, f, sdf = sdf_meshing.create_mesh(
                 sdf_decoder,
                 effective_file_name,
@@ -78,15 +81,14 @@ def generate_meshes(weights, folder_name="meshes", info="0", res=64, level=0):
             sdfs.append(sdf)
             mesh = trimesh.Trimesh(v, f)
             meshes.append(mesh)
-
         sdfs = torch.stack(sdfs)
         return meshes, sdfs
 
-def generate_images_from_VAE(weights):
+def generate_images_from_VAE(x_0):
     out_imgs = []
     if not os.path.isdir(mesh_dir):
         os.makedirs(f"gen_meshes/VAE")
-    mesh, _ = generate_meshes(weights=weights.unsqueeze(0), res=700)
+    mesh, _ = generate_meshes(x_0.unsqueeze(0), None, res=700)
     mesh = mesh[0]
     mesh.vertices *= 2
     mesh.export(f"gen_meshes/VAE/mesh_{len(out_imgs)}.obj")
@@ -135,8 +137,8 @@ if __name__ == "__main__":
     N_dist.scale = N_dist.scale.to(device)
 
     for i in range(num_imgs):
-        weights = sample_from_VAE(model, N_dist)
-        img = generate_images_from_VAE(weights)
+        x_0 = sample_from_VAE(model, N_dist)
+        img = generate_images_from_VAE(x_0)
         print(img)
         '''wandb_logger.log_image(
                 "generated_renders", img, step=i
