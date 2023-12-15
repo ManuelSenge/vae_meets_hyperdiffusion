3bdcaea1b9259bfe1e79969a2d46b0f98c019dc4
Julian <julian.streit1@gmail.com>
2023-12-15 01:16:48 +0100

    Add ldm model as an option

--- src/ldm_autoencoder/main.py
+++ src/ldm_autoencoder/main.py
@@ -1,7 +1,5 @@
-from model.openaimodel import UNetModel
-
-#from model_linear import VariationalAutoencoder
-from loss import VAELoss
+from model.ldm.modules.autoencoder import AutoencoderKL
+from omegaconf import OmegaConf
 import torch.nn as nn
 import torch
 from torch.utils.data import DataLoader
@@ -16,12 +14,12 @@ import numpy as np
 import sys
 sys.path.append('../')
 from dataset import WeightDataset
-from sampler import SingleInstanceBatchSampler
 from hd_utils import Config
 from omegaconf import DictConfig
 import hydra
 from torchsummary import summary
-import datetime
+from model.ldm.modules.autoencoder import AutoencoderKL
+from loss import VAELoss
 
 # need this seed for the lookup (as data is randomly shuffled)
 random.seed(1234)
@@ -35,29 +33,16 @@ np.random.seed(1234)
 def main(cfg: DictConfig):
     Config.config = cfg
     cfg.filter_bad_path = '../' + cfg.filter_bad_path
-    
     output_dir = '/hyperdiffusion/output_files'
-    attention_encoder = "0000"
-    attention_decoder = attention_encoder[::-1]
     BS = 32
     SEED = 1234
-    N_EPOCHS = 500
+    N_EPOCHS = 100
     warmup_epochs = 10
-    
-    adam_betas=(0.8, 0.99)
-
-    variational = False
     normalize = True
     learning_rate = 0.0002
-    attention_resolutions  = [8, 16]
-    # attention_resolutions  = []
-    dropout = 0.2
-    num_res_blocks = 3
-    channel_mult = [1, 4, 8, 16, 32]
-    single_sample_overfit = False
-
     device = "auto"
-    lob_wandb = 1
+    log_wandb = 1
+    variational = False # True to make VAE variational False to make it an AE
     if device == 'auto':
         if torch.cuda.is_available():
             device = torch.device('cuda')
@@ -66,12 +51,7 @@ def main(cfg: DictConfig):
         else:
             device = torch.device('cpu')
 
-
-    date_str = datetime.datetime.now().strftime("%Y-%m-%d")
-    run_params_name = f'{date_str}_unet_2298__lr_{learning_rate}_attention_res_{str(attention_resolutions)}_dropout_{dropout}' #f'bn_lr{learning_rate}_E{attention_encoder}_num_att_layers{num_att_layers}_enc_chans{wandb_enc_channels}_enc_kernel_sizes{wandb_enc_kernel_sizes}_warmup_epochs{warmup_epochs}'
-
-    if single_sample_overfit:
-        run_params_name = 'single_sample_overfit_' + run_params_name
+    run_params_name = f'ldm_latent_1149_attention_[1149]_dropout_0.2_lr_0.0002_num_res_3_normalized_[1,4,8,16,32,64]' #f'bn_lr{learning_rate}_E{attention_encoder}_num_att_layers{num_att_layers}_enc_chans{wandb_enc_channels}_enc_kernel_sizes{wandb_enc_kernel_sizes}_warmup_epochs{warmup_epochs}'
 
     output_file = f'{run_params_name}_{SEED}'
     checkpoint_path = output_dir
@@ -79,28 +59,23 @@ def main(cfg: DictConfig):
     random.seed(SEED)
     np.random.seed(SEED)
 
-    
 
-    if lob_wandb:
-        project = "UNet"
+    if log_wandb:
+        project = "LDM"
 
         wandb.init( project=project,
                     entity="adl-cv",
-                    name=f'first_test_{run_params_name}',
-                    group=f'first_test',
+                    name=f'{run_params_name}',
                     config={
-                    "latent_attention": True,
-                    "attention_resolutions": attention_resolutions,
-                    "dropout": dropout,
                     "learning_rate": learning_rate,
-                    "num_res_blocks":  num_res_blocks,
                     "batch_size": BS,
                     "SEED": SEED,
                     "epochs": N_EPOCHS,
-                    "channel_mult": channel_mult,
-                    "single_sample_overfit": single_sample_overfit,
-                    "adam_betas": adam_betas,
-                    "normalize": normalize
+                    "normalize": True,
+                    "ch_mult": [ 1,4,8,16,32,64],
+                    "dropout": 0.2,
+                    "num_res_block": 3,
+                    "latent": 1149
                     })
 
     dataset_path = os.path.join('..', Config.config["dataset_dir"], Config.config["dataset"])
@@ -139,6 +114,8 @@ def main(cfg: DictConfig):
         print("OAI-Coeff Normalization: ", oai_coeff)
     normalizing_constant = oai_coeff if oai_coeff else 1
 
+    print("OAI-Coeff Normalization: ", normalizing_constant)
+
     # create dataset and dataloader
     print('create dataset...')
     train_dt = WeightDataset(
@@ -151,25 +128,13 @@ def main(cfg: DictConfig):
         normalize=normalizing_constant
     )
 
-    train_dl = None
-    if single_sample_overfit:
-        train_dl = DataLoader(
-            train_dt,
-            sampler = SingleInstanceBatchSampler(len(train_dt)),
-            batch_size=BS,
-            num_workers=1,
-            pin_memory=True
-        )
-    else: 
-        train_dl = DataLoader(
-            train_dt,
-            batch_size=Config.get("batch_size"),
-            shuffle=True,
-            num_workers=1,
-            pin_memory=True,
-        )
-
-    assert train_dl is not None
+    train_dl = DataLoader(
+        train_dt,
+        batch_size=Config.get("batch_size"),
+        shuffle=True,
+        num_workers=1,
+        pin_memory=True,
+    )
 
     val_dt = WeightDataset(
         mlps_folder_train,
@@ -178,7 +143,7 @@ def main(cfg: DictConfig):
         mlp_kwargs,
         cfg,
         val_object_names,
-        normalize=normalizing_constant
+        normalize=normalizing_constant,
     )
 
     val_dl = DataLoader(
@@ -187,7 +152,6 @@ def main(cfg: DictConfig):
         shuffle=True,
         num_workers=1,
         pin_memory=True,
-        # drop_last=True
     )
 
     test_dt = WeightDataset(
@@ -206,34 +170,28 @@ def main(cfg: DictConfig):
         shuffle=True,
         num_workers=1,
         pin_memory=True,
-        drop_last=True
     )
 
     # create model loss and optimizer
     #model = VariationalAutoencoder(latent_dims=512, device=device)
     print('create model..')
-    model = UNetModel(image_size=36744, 
-                    in_channels=1, 
-                    model_channels=1, 
-                    out_channels=1, 
-                    num_res_blocks=num_res_blocks,
-                    attention_resolutions=attention_resolutions,
-                    dropout=dropout,
-                    channel_mult=channel_mult,
-                    num_heads=1,
-                    dims=1,
-                    num_head_channels=-1)
-    #device = 'cpu'
-    #print(summary(model, (1, 36737), device="cpu"))
+    config_model = OmegaConf.load('./model/autoencoder_kl_8x8x64.yaml')
+    loss_config = config_model.model.params.lossconfig
+    ddconfig = config_model.model.params.ddconfig
+    model = AutoencoderKL(ddconfig=ddconfig, lossconfig=loss_config, embed_dim=1149)
+    # loss = model.loss
+
+    loss = VAELoss(autoencoder=None)
+
+    # print(summary(model, (1, 36768), device="cpu"))
 
     model = model.to(device)
     print(f'model created..')
 
-    loss = VAELoss(model)
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, betas=adam_betas)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
     # load checkpoint if necessary
-    '''checkpoint_path = f"{c_dir}/model_dict{SEED}.pt"
+    '''checkpoint_path = f"{output_dir}/model_dict{SEED}.pt"
     if os.path.exists(checkpoint_path):
         checkpoint = torch.load(checkpoint_path)
         model.load_state_dict(checkpoint['model_state_dict'])
@@ -250,30 +208,29 @@ def main(cfg: DictConfig):
 
     for epoch in range(start_epoch, N_EPOCHS):
         start_time = time.time()
-        train_mse_loss, train_kl_loss = train(model, train_dl, optimizer, loss, device, epoch <= warmup_epochs, variational=variational)
-        val_mse_loss, val_kl_loss = evaluate(model, val_dl, loss, device, variational=variational)
+        train_mse_loss, train_kl_loss = train(model, train_dl, optimizer, loss, device, epoch <= warmup_epochs, variational=variational, normalizing_constant=normalizing_constant)
+        val_mse_loss, val_kl_loss = evaluate(model, val_dl, loss, device, variational=variational, normalizing_constant=normalizing_constant)
         end_time = time.time()
         epoch_mins, epoch_secs = epoch_time(start_time, end_time)
 
-        if lob_wandb:
-            wandb.log({"train/mse_loss": train_mse_loss/normalizing_constant, "train/kl_loss": train_kl_loss,\
-                "val/mse_loss": val_mse_loss /normalizing_constant, "val/kl_loss":val_kl_loss})
+        if log_wandb:
+            wandb.log({"train/mse_loss": train_mse_loss, "train/kl_loss": train_kl_loss,\
+                "val/mse_loss": val_mse_loss, "val/kl_loss":val_kl_loss})
 
         if val_mse_loss+val_kl_loss < best_valid_loss:
             best_valid_loss = val_mse_loss+val_kl_loss
             torch.save(model.state_dict(), f"{output_dir}/{output_file}.pt")
-
-            if lob_wandb:
-                wandb.save(f"{output_dir}/{output_file}.pt", policy="now")
+            if log_wandb:
+                wandb.save(f"{output_dir}/{output_file}.pt")
 
         print(f'Epoch: {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
-        print(f'\t Train MSE Loss: {train_mse_loss /normalizing_constant:.3f} Train KL Loss: {train_kl_loss:.3f}')
-        print(f'\t Val. MSE Loss: {val_mse_loss / normalizing_constant:.3f} Val. KL Loss: {val_kl_loss:.3f}')
+        print(f'\t Train MSE Loss: {train_mse_loss:.3f} Train KL Loss: {train_kl_loss:.3f}')
+        print(f'\t Val. MSE Loss: {val_mse_loss:.3f} Val. KL Loss: {val_kl_loss:.3f}')
 
         f = open(f"{output_dir}/{output_file}.txt", "a")
         f.write(f'Epoch: {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s\n')
-        f.write(f'\tTrain MSE Loss: {train_mse_loss / normalizing_constant:.3f} Train KL Loss: {train_kl_loss:.3f}\n')
-        f.write(f'\tVal. MSE Loss: {val_mse_loss / normalizing_constant:.3f} Val. KL Loss: {val_kl_loss:.3f}\n')
+        f.write(f'\tTrain MSE Loss: {train_mse_loss:.3f} Train KL Loss: {train_kl_loss:.3f}\n')
+        f.write(f'\tVal. MSE Loss: {val_mse_loss:.3f} Val. KL Loss: {val_kl_loss:.3f}\n')
         f.close()
         
         # save model and optimizer
@@ -287,9 +244,9 @@ def main(cfg: DictConfig):
 
     # load the best model and test it on the test set
     model.load_state_dict(torch.load(f"{output_dir}/{output_file}.pt"))
-    test_mse_loss, test_kl_loss = evaluate(model, test_dl, loss, device, variational=variational)
+    test_mse_loss, test_kl_loss = evaluate(model, test_dl, loss, device, variational=variational, normalizing_constant=normalizing_constant)
 
-    if lob_wandb:
+    if log_wandb:
         wandb.log({"test/mse_loss": test_mse_loss, "test/kl_loss":test_kl_loss})
 
     print(f'Test MSE Loss: {test_mse_loss:.3f} Test KL Loss: {test_kl_loss:.3f}')
---
+++ src/ldm_autoencoder/train_eval.py
@@ -0,0 +1,56 @@
+
+import torch
+
+def train(model, iterator, optimizer, loss, device, warmup, variational, normalizing_constant=1):
+    epoch_loss_val_mse = 0
+    epoch_loss_val_kl = 0
+    
+    model.train()
+    for i, batch in enumerate(iterator): # batch is simply a batch of ci-matricies as a tensor as x and y are the same 
+        # attention_mask, base_ids are already on device
+        weights, weights_prev, weights_prev = batch
+        weights = weights.view(weights.shape[0], 1, weights.shape[1])
+        weights = torch.nn.functional.pad(weights, (0, 31)).to(device)
+        optimizer.zero_grad() # clear gradients first
+
+        predictions, posterior = model(weights)
+
+        loss_val_mse, loss_val_kl = loss(predictions, weights, variational)
+        # during warmup only train mse loss
+        if warmup or not variational:
+            loss_val = loss_val_mse
+        else:
+            loss_val = loss_val_mse + loss_val_kl
+            
+        loss_val.backward()
+        optimizer.step()
+        
+        epoch_loss_val_mse += loss_val_mse.item()
+        epoch_loss_val_kl += loss_val_kl.item()
+            
+
+    return epoch_loss_val_mse / (len(iterator) * normalizing_constant), epoch_loss_val_kl / len(iterator)
+
+def evaluate(model, iterator, loss, device, variational, normalizing_constant=1):
+    epoch_loss_val_mse = 0
+    epoch_loss_val_kl = 0
+    
+    model.eval()
+    for i, batch in enumerate(iterator): # batch is simply a batch of ci-matricies as a tensor as x and y are the same 
+        # attention_mask, base_ids are already on device
+        weights, weights_prev, weights_prev = batch
+        weights = weights.view(weights.shape[0], 1, weights.shape[1])
+        weights = torch.nn.functional.pad(weights, (0, 31)).to(device)
+
+        predictions, posterior = model(weights)
+
+        loss_val_mse, loss_val_kl = loss(predictions, weights, variational)
+        if not variational:
+            loss_val = loss_val_mse
+        else:
+            loss_val = loss_val_mse + loss_val_kl
+  
+        epoch_loss_val_mse += loss_val_mse.item()
+        epoch_loss_val_kl += loss_val_kl.item()
+
+    return epoch_loss_val_mse / (len(iterator) * normalizing_constant), epoch_loss_val_kl / len(iterator)
